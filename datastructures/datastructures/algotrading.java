c5019a931943b5d35168813d881fd425cd07b1e0
tyman2323 <122325523+tyman2323@users.noreply.github.com>
2023-05-02 17:14:43 -0400

    Delete datastructures directory

--- datastructures/datastructures/algotrading.java
+++
@@ -1,4 +0,0 @@
-//in this program you are to sort multiple linked lists into one globally sort, try not to bring them all in one and then sort becuase that will be o(nlogn)
-public class algotrading{
-
-}
